de2674009c05a3f0e0248be5c1117e6008f34c04
dakotaruelle <17259752+dakotaruelle@users.noreply.github.com>
2023-02-28 13:19:47 -0600

    4 files modified

--- App.tsx
+++ App.tsx
@@ -1,29 +1,23 @@
-import * as React from 'react';
 import './style.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Page1 } from './page-1';
 import { Page2 } from './page-2';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Page1 />,
   },
   {
-    path: "/page-1",
+    path: '/page-1',
     element: <Page1 />,
   },
   {
-    path: "/page-2",
+    path: '/page-2',
     element: <Page2 />,
   },
 ]);
 
 export function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
--- nav.tsx
+++ nav.tsx
@@ -1,11 +1,9 @@
 import { Button, Divider } from '@mui/material';
-import * as React from 'react';
-import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 export function Nav() {
   return (
-    <Fragment>
+    <>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Link
           to="/page-1"
@@ -19,6 +17,6 @@ export function Nav() {
       </div>
 
       <Divider />
-    </Fragment>
+    </>
   );
 }
