29eaa94aa9d4936f04a82afe17404bd4087ad0ca
dakotaruelle <17259752+dakotaruelle@users.noreply.github.com>
2023-02-28 14:03:19 -0600

    1 file modified

--- index.tsx
+++ index.tsx
@@ -6,12 +6,22 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#005fe5' },
+  },
+});
+
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
